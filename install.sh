4687d19bafc733bbd21605d0c6832b65da13e109
Dennis Johannisson <mail@gawetaner.de>
2020-11-03 10:44:10 +0100

    Replace deprecated brew installer with the new one

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 
 # Install Homebrew
 if ! [ -x "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install tools and apps with brew bundle
